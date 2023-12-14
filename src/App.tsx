d0342491de291a64c46a086cdc8b3729fe40f789
Elisa <azenagelisa@gmail.com>
2023-12-14 12:02:58 +0100

    allow origin back url

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App()
 {
 
 	axios.defaults.withCredentials = true
-	axios.defaults.headers.common['Access-Control-Allow-Origin'] = `http://localhost:3333`;
+	axios.defaults.headers.common['Access-Control-Allow-Origin'] = `${process.env.REACT_APP_BACK_URL}`;
 
 	const { user, updateUser, setIsLoaded, setIsConnected, isSimpleConnected, isConnected, isLoaded, setIsSimpleConnected } = useContext(UserContext);
 
